0d2d561b72a12ca8aca4a4b81a85b9eed1a64cd9
MRubi0 <85171608+MRubi0@users.noreply.github.com>
2023-04-27 01:34:25 +0100

    mas ajustes

--- LetsTourTec/urls.py
+++ LetsTourTec/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from django.views import vista_registro
+from LTtApp.views import vista_registro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
