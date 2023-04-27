005dd0b8c1fc1daf32957d09fe984ba687950157
MRubi0 <85171608+MRubi0@users.noreply.github.com>
2023-04-27 11:33:02 +0100

    log

--- LetsTourTec/urls.py
+++ LetsTourTec/urls.py
@@ -16,13 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 from LTtApp.views import vista_registro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', vista_registro.as_view(), name='signup'),
-    path('LTtApp/', include('LTtApp.urls')),
+    path('', include('LTtApp.urls')),
     ]
