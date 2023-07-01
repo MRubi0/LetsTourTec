367b578f906a5d9c09610b006a6630896aea23f9
MRubi0 <miguel_ortiz_rejon@hotmail.com>
2023-07-01 01:52:58 +0100

    Corregido error en INSTALLED_APPS

--- LetsTourTec/settings.py
+++ LetsTourTec/settings.py
@@ -36,9 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'storages'
     'LTtApp',
+    'django.contrib.staticfiles',
+    'storages',
+    
     #'django.contrib.gis',
 ]
 
