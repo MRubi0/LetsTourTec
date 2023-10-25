b89ff22d7d166d7c3b51b2ea86d0d3d49de74cbd
MRubi0 <miguel_ortiz_rejon@hotmail.com>
2023-10-25 22:44:10 +0100

    botones del principio

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -7,6 +7,11 @@ import { LatestToursService } from 'src/app/services/latest-tours.service';
   styleUrls: ['./cards.component.scss']
 })
 export class CardsComponent {
+  title: string = 'Closest tours'; // valor inicial
+  button1Text: string = 'Show last tours'; // valor inicial
+  button2Text: string = 'Update location'; // valor inicial
+  button3Text: string = 'Show random tours'; // valor inicial
+
 
   lastTours: any;
 
@@ -20,13 +25,29 @@ export class CardsComponent {
   lastToursF() {
     this.latestToursService.getLastestTours().subscribe((data: any) => {
       this.lastTours = data;
+    this.title = 'Last Tours uploaded';
+    this.button1Text = 'Update tours';
+    this.button2Text = 'Show closest tours';
+    this.button3Text = 'Show random tours';
     });
   }
   randomToursF() {
     this.latestToursService.getRadomTours().subscribe((data: any) => {
       this.lastTours = data;
+    this.title = 'Random Tours';
+    this.button1Text = 'Show last tours';
+    this.button2Text = 'Show closest tours';
+    this.button3Text = 'Show random tours';
     });
   }
+  closestToursF() {
+    this.getCoordenades();
+    this.title = 'Closest Tours';
+    this.button1Text = 'Show last tours';
+    this.button2Text = 'Update location';
+    this.button3Text = 'Show random tours';
+  }
+  
 
   getCoordenades() {
     if ("geolocation" in navigator) {
