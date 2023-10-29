7e2940095a696ea2bd94efbfd2ad6df6605d45c8
MRubi0 <miguel_ortiz_rejon@hotmail.com>
2023-10-29 01:09:13 +0100

    form register updated

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,25 +1,132 @@
+
+// import { Component } from '@angular/core';
+// import { FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
+// import { HttpClient } from '@angular/common/http';
+
+
+// function passwordMatchValidator(g: AbstractControl) {
+//   const passwordControl = g.get('password');
+//   const repeatPasswordControl = g.get('repeatPassword');
+
+//   if (!passwordControl || !repeatPasswordControl) {
+//       return null;
+//   }
+
+//   return passwordControl.value === repeatPasswordControl.value
+//       ? null : {'mismatch': true};
+// }
+
+// @Component({
+//   selector: 'app-Signup',
+//   templateUrl: './Signup.component.html',
+//   styleUrls: ['./Signup.component.scss']
+// })
+// export class SignupComponent {
+
+//   public serverErrors: any = {};
+
+
+//   authenticationError = false;
+
+//   loginForm = new FormGroup({
+//     firstName: new FormControl('', [Validators.required]),
+//     lastName: new FormControl('', [Validators.required]),
+//     email: new FormControl('', [Validators.required, Validators.email]),
+//     password: new FormControl('', [Validators.required]),
+//     repeatPassword: new FormControl('', [Validators.required])}
+//     , { validators: passwordMatchValidator });
+
+//   constructor(private http: HttpClient) {}
+
+//   registerUser() {
+//     const formData = this.loginForm.value;
+  
+//     this.http.post('http://localhost:8000/register/', formData).subscribe({
+//       next: (response: any) => {
+//         if (response.success) {
+//           console.log("Success");
+//         } else {
+//           this.serverErrors = response.errors;
+//           this.authenticationError = true;
+//         }
+//       },
+//       error: (error) => {
+//         console.error('Hubo un error en la petición:', error);
+//       },
+//       complete: () => {
+//         console.log('bien');
+//       }
+//     });
+//   }
+
+//   ngOnInit(){
+//     this.loginForm.valueChanges.subscribe((data:any)=>{
+//       console.log('data --->', data);
+//     });
+//   }
+// }
+
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+function passwordMatchValidator(g: AbstractControl) {
+    const passwordControl = g.get('password1'); // Ajustado a "password1"
+    const repeatPasswordControl = g.get('password2'); // Ajustado a "password2"
+
+    if (!passwordControl || !repeatPasswordControl) {
+        return null;
+    }
+
+    return passwordControl.value === repeatPasswordControl.value
+        ? null : {'mismatch': true};
+}
 
 @Component({
-  selector: 'app-Signup',
-  templateUrl: './Signup.component.html',
-  styleUrls: ['./Signup.component.scss']
+    selector: 'app-Signup',
+    templateUrl: './Signup.component.html',
+    styleUrls: ['./Signup.component.scss']
 })
 export class SignupComponent {
 
-  authenticationError = false;
+    public serverErrors: any = {};
 
-  loginForm = new FormGroup({
-    username: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-    rememberMe: new FormControl(false, [Validators.required]),
-  });
+    authenticationError = false;
 
-  ngOnInit(){
-    this.loginForm.valueChanges.subscribe((data:any)=>{
-      console.log('data --->', data);
-    });
+    loginForm = new FormGroup({
+        first_name: new FormControl('', [Validators.required]), // Ajustado a "first_name"
+        last_name: new FormControl('', [Validators.required]),  // Ajustado a "last_name"
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password1: new FormControl('', [Validators.required]),  // Ajustado a "password1"
+        password2: new FormControl('', [Validators.required])  // Ajustado a "password2"
+    }, { validators: passwordMatchValidator });
 
-  }
+    constructor(private http: HttpClient) { }
+
+    registerUser() {
+        const formData = this.loginForm.value;
+
+        this.http.post('http://localhost:8000/register/', formData).subscribe({
+            next: (response: any) => {
+                if (response.success) {
+                    console.log("Success");
+                } else {
+                    this.serverErrors = response.errors;
+                    this.authenticationError = true;
+                }
+            },
+            error: (error) => {
+                console.error('Hubo un error en la petición:', error);
+            },
+            complete: () => {
+                console.log('bien');
+            }
+        });
+    }
+
+    ngOnInit() {
+        this.loginForm.valueChanges.subscribe((data: any) => {
+            console.log('data --->', data);
+        });
+    }
 }
