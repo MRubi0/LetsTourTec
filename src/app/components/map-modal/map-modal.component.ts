5dd96cd4052465f8e3643a3a826ba27fab15c94e
MRubi0 <miguel_ortiz_rejon@hotmail.com>
2023-11-22 23:14:06 +0000

    map modal

--- src/app/components/map-modal/map-modal.component.ts
+++ src/app/components/map-modal/map-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
 import * as L from 'leaflet';
 import 'leaflet-routing-machine';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
@@ -8,8 +8,12 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   templateUrl: './map-modal.component.html',
   styleUrls: ['./map-modal.component.scss']
 })
-export class MapModalComponent implements OnInit {
-  
+export class MapModalComponent implements OnInit, OnDestroy {
+  private map!: L.Map;
+private control!: L.Routing.Control;
+private watchId!: number;
+
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<MapModalComponent>
@@ -20,6 +24,13 @@ export class MapModalComponent implements OnInit {
   ngOnInit(): void {
     this.initializeMap();
   }
+
+  ngOnDestroy(): void {
+    if (this.watchId) {
+      navigator.geolocation.clearWatch(this.watchId);
+    }
+  }
+
   closeModal(): void {
     this.dialogRef.close();
   }
@@ -29,46 +40,67 @@ export class MapModalComponent implements OnInit {
       const userLat = position.coords.latitude;
       const userLng = position.coords.longitude;
 
-      console.log('Initializing map with user latitude:', userLat, 'and user longitude:', userLng);
-      console.log('Destination latitude:', this.data.latitude, 'and longitude:', this.data.longitude);
+      this.map = L.map('map').setView([userLat, userLng], 13);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+      }).addTo(this.map);
 
-      if (this.data.latitude != null && this.data.longitude != null) {
-        const map = L.map('map').setView([userLat, userLng], 13);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-        }).addTo(map);
-  
-        const control = L.Routing.control({
-          waypoints: [
-            L.latLng(userLat, userLng),
-            L.latLng(this.data.latitude, this.data.longitude)
-          ],
-          routeWhileDragging: true,
+      this.control = L.Routing.control({
+        waypoints: [
+          L.latLng(userLat, userLng),
+          L.latLng(this.data.latitude, this.data.longitude)
+        ],
+        routeWhileDragging: true,
         collapsible: true, 
         show: true,  
         addWaypoints: false, 
-        }).addTo(map);
-  
-        control.on('routesfound', function(e) {
-          
-          const waypoints = e.waypoints;
-          const startMarker = L.marker(waypoints[0].latLng, { draggable: false }).addTo(map);
-          const endMarker = L.marker(waypoints[waypoints.length - 1].latLng, { draggable: false }).addTo(map);
-          
-          
-          [startMarker, endMarker].forEach(marker => {
-            marker.on('add', function() {
-              const element = marker.getElement();
-              if (element) {
-                L.DomUtil.removeClass(element, 'leaflet-marker-draggable');
-                L.DomUtil.removeClass(element, 'leaflet-interactive');
-              }
-            });
+      }).addTo(this.map);
+
+      this.control.on('routesfound', (e) => {
+        const waypoints = e.waypoints;
+        const startMarker = L.marker(e.waypoints[0].latLng, { draggable: false }).addTo(this.map);
+  const endMarker = L.marker(e.waypoints[e.waypoints.length - 1].latLng, { draggable: false }).addTo(this.map);
+
+        [startMarker, endMarker].forEach(marker => {
+          marker.on('add', function() {
+            const element = marker.getElement();
+            if (element) {
+              L.DomUtil.removeClass(element, 'leaflet-marker-draggable');
+              L.DomUtil.removeClass(element, 'leaflet-interactive');
+            }
           });
         });
-      } else {
-        console.log('Latitude or longitude is null');
-      }
+      });
+
+      // Configuración del seguimiento de posición del usuario
+      this.watchId = navigator.geolocation.watchPosition((newPosition) => {
+        const newLat = newPosition.coords.latitude;
+        const newLng = newPosition.coords.longitude;
+
+        // Actualizar la primera waypoint con la nueva posición del usuario
+        if (this.control) {
+          const newWaypoint = L.Routing.waypoint(L.latLng(newLat, newLng));
+const waypoints = this.control.getWaypoints();
+waypoints[0] = newWaypoint;
+this.control.setWaypoints(waypoints);
+
+        }
+      }, (error) => {
+        console.error(error);
+      }, {
+        enableHighAccuracy: true,
+        maximumAge: 10000,
+        timeout: 5000
+      });
+
+    }, (error) => {
+      console.error('Error getting user position:', error);
+    }, {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
     });
   }
+
+  // Otros métodos del componente...
 }
