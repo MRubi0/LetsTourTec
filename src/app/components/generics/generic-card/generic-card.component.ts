42792b23330cfc7190e72af58ebe8c6f4d2f4ac6
MRubi0 <miguel_ortiz_rejon@hotmail.com>
2023-11-24 23:37:04 +0000

    vista cards show more show less

--- src/app/components/generics/generic-card/generic-card.component.ts
+++ src/app/components/generics/generic-card/generic-card.component.ts
@@ -7,6 +7,7 @@ import { SharedService } from 'src/app/services/shared.service';
   styleUrls: ['./generic-card.component.scss']
 })
 export class GenericCardComponent {
+  public showFullDescription: { [key: number]: boolean } = {};
 
   @Input('toursdata') toursdata:any=[];
   @Input('all-tours') view!:boolean;
@@ -20,6 +21,9 @@ export class GenericCardComponent {
   constructor(private sharedService:SharedService){}
 
   ngOnChanges(){
+    this.toursdata.forEach((tour: any) => {
+      this.showFullDescription[tour.id] = false;
+  });
     this.toursdata.map((data:any)=>{
       
       const partofUrl = data.imagen.url.split('/'); // Divide la URL en partes usando '/' como separador
@@ -43,4 +47,13 @@ export class GenericCardComponent {
     console.log('image -->', image);
     //this.sharedService.setImage=image;
   }
+  stopClickPropagate(event: Event): void {
+    event.stopPropagation();
+}
+toggleDescription(event: Event, tourId: number): void {
+  event.stopPropagation();
+  this.showFullDescription[tourId] = !this.showFullDescription[tourId];
+}
+
+
 }
