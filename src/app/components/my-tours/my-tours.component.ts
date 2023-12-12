7ff754ad66c6e25186348f6d48aa89527ed34f41
MRubi0 <miguel_ortiz_rejon@hotmail.com>
2023-12-12 02:11:26 +0000

    cambios rama nueva

--- src/app/components/my-tours/my-tours.component.ts
+++ src/app/components/my-tours/my-tours.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from 'src/app/services/auth.service';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-my-tours',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-tours.component.scss']
 })
 export class MyToursComponent {
+  tours: any[] = [];
 
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  ngOnInit() {
+      this.loadTours();
+  }
+
+  loadTours(): void {
+    const accessToken = this.authService.getToken();
+    if (accessToken) {
+        const decodedToken: any = jwtDecode(accessToken);
+        const userId = decodedToken.user_id;  
+        this.http.get(`http://localhost:8000/api/get_user_tours?id=${userId}`).subscribe(data => {
+            this.tours = (data as any)['tours'];
+        }, error => {
+            console.error('Error al cargar los tours:', error);
+        });
+    }
+  }
 }
