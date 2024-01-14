1f4d889be7333876327cdda715459defd0a7e2ba
MRubi0 <miguel_ortiz_rejon@hotmail.com>
2024-01-14 21:52:31 +0000

    env

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -5,7 +5,8 @@ export const environment = {
   }
 
 
-/*   export const environment = {
+/*   
+export const environment = {
     production : false, 
     apiUrl:"http://127.0.0.1:8000/",
     bucket:"https://letstourtec-bucket-2.s3.eu-north-1.amazonaws.com"
