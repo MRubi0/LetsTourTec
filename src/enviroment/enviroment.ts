e117ac8f8d3897bfabbb7e47f7990416d1afa59e
Juan <juansebs99@hotmail.com>
2024-01-15 10:06:00 -0500

    ajustes mapas usando Graphhoper prueba

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production : false, 
-    apiUrl:"https://letstourtec-c393a22f9c2b.herokuapp.com/",
-    bucket:"https://letstourtec-bucket-2.s3.eu-north-1.amazonaws.com"
+    //apiUrl:"https://letstourtec-c393a22f9c2b.herokuapp.com/",
+    apiUrl:"http://127.0.0.1:8000/",
+    bucket:"https://letstourtec-bucket-2.s3.eu-north-1.amazonaws.com",
+    graph:"https://graphhopper.com/api/1/route"
   }
