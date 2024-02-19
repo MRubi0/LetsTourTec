a9ae23a23f97174787f20bd1478ed2505a11af80
MRubi0 <miguel_ortiz_rejon@hotmail.com>
2024-02-19 23:18:13 +0000

    formulario salida

--- src/app/components/exit/exit.component.ts
+++ src/app/components/exit/exit.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/enviroment/enviroment';
 
 @Component({
   selector: 'app-exit',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./exit.component.scss']
 })
 export class ExitComponent {
+  formData: FormData = new FormData();
 
+  constructor(private http: HttpClient) {}
+
+  submitSurvey() {
+    // Obtener el formulario
+    const form = document.querySelector('form');
+
+    // Verificar si el formulario existe
+    if (form) {
+      // Crear FormData solo si se encontró el formulario
+      this.formData = new FormData(form);
+
+      // Enviar los datos del formulario utilizando HttpClient
+      this.http.post<any>(`${environment.apiUrl}encuesta/`, this.formData)
+        .subscribe(
+          data => {
+            console.log('Datos enviados con éxito:', data);
+            // Aquí puedes manejar la respuesta si es necesario
+          },
+          error => {
+            console.error('Error al enviar los datos:', error);
+          }
+        );
+    } else {
+      console.error('No se encontró ningún formulario en la página');
+    }
+  }
 }
--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -1,15 +1,16 @@
+/*
+
 export const environment = {
     production : false, 
     apiUrl:"https://letstourtec-c393a22f9c2b.herokuapp.com/",
     bucket:"https://letstourtec-bucket-2.s3.eu-north-1.amazonaws.com"
   }
 
-
-/* 
+*/
+ 
 export const environment = {
     production : false, 
     apiUrl:"http://127.0.0.1:8000/",
     bucket:"https://letstourtec-bucket-2.s3.eu-north-1.amazonaws.com"
 
   } 
-*/
