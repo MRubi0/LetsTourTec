755da00d48e4fb896f2d1aca05a569083b4646fe
Juan <juansebs99@hotmail.com>
2024-08-05 20:48:46 -0500

    ajustes en generic card y history tours

--- src/app/components/generics/generic-card/generic-card.component.ts
+++ src/app/components/generics/generic-card/generic-card.component.ts
@@ -41,16 +41,7 @@ export class GenericCardComponent {
 
   ngOnChanges() {
     this.toursdata.forEach((tour: any) => {
-      this.showFullDescription[tour.id] = false;
-  
-      this.sharedService.getMediaValoraciones(tour.id).subscribe({
-        next: (response:any) => {
-          tour.mediaValoracion = response.media_puntuacion;
-        },
-        error: (error:any) => {
-          console.error('Error al obtener la media de valoraciones:', error);
-        }
-      });
+      this.showFullDescription[tour.id] = false;     
     });
     this.toursdata.map((data:any)=>{
       if(data.tipo_de_tour=='ocio'){
