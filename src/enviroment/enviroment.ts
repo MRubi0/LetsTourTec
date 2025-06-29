3189363d28d74ce359a09c32be33abba11231364
MRubi0 <miguel_ortiz_rejon@hotmail.com>
2025-06-29 04:14:03 +0100

    env

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -1,5 +1,5 @@
 
-
+/*
   export const environment = {
     production : false, 
     apiUrl:'http://35.177.186.168/api/', 
@@ -7,8 +7,14 @@
   }
  
 
-  /*export const environment = {
+  export const environment = {
     production : false, 
     apiUrl:"http://127.0.0.1:8000/",
     bucket:"https://bucket-test-west2.s3.eu-west-2.amazonaws.com"
   }*/
+
+    export const environment = {
+      production : false, 
+      apiUrl:'https://letstourtec.app/api/', 
+      bucket:"https://bucket-test-west2.s3.eu-west-2.amazonaws.com"
+    }
